9f64bf94fdf4e478c0ae1b5aeb2ff84231d038ba
masazdream <masai@masai-no-MacBook-Pro.local>
2015-12-08 01:56:36 +0900

    add cnn using tensorflow structure.

---
+++ CNN_tiny/cifar10_model.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+# tensorflow
+import tensorflow as tf
+
+# settings
+import cifar10_settings as settings
+FLAGS = settings.FLAGS
+
+IMAGE_SIZE = FLAGS.image_size
+NUM_CLASSES = FLAGS.num_classes
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = FLAGS.num_examples_per_epoch_for_train
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = FLAGS.num_examples_per_epoch_for_eval
+
+MOVING_AVERAGE_DECAY = FLAGS.moving_average_decay
+NUM_EPOCHS_PER_DECAY = FLAGS.num_epochs_per_decay
+LEARNING_RATE_DECAY_FACTOR = FLAGS.learning_rate_decay_factor
+INITIAL_LEARNING_RATE = FLAGS.learning_rate
+
+TOWER_NAME = FLAGS.tower_name
+
+
+def test():
+    pass
+
+if __name__ == '__main__':
+    test()

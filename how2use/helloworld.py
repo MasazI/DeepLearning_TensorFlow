3cd2bf8e281020b85236e0acda5e1495c6fa1058
masazdream <masai@masai-no-MacBook-Pro.local>
2015-12-08 20:39:41 +0900

    add how2use string_input_producer

--- how2use/helloworld.py
+++ how2use/helloworld.py
@@ -1,22 +1,38 @@
 #coding: utf-8
 
 import tensorflow as tf
+
+# how2use Session
 hello = tf.constant('Hello, TensorFlow!!!')
 sess = tf.Session()
-
 print sess.run(hello)
-
 a = tf.constant(10)
 print sess.run(a)
-
 b = tf.constant(20)
-
 print sess.run(a+b)
 
+# how2use match_filenames_once
 isess = tf.InteractiveSession()
 files = tf.train.match_filenames_once('/Users/masai/source/tensorflow/how2use/[0-9].txt')
-
 # 変数の初期化
 isess.run(tf.initialize_all_variables())
 print isess.run(files)
 
+# how2use string_input_producer
+filename_queue = tf.train.string_input_producer(["reader.txt", "reader2.txt"])
+reader = tf.TextLineReader() # いろんなリーダがある
+key, value = reader.read(filename_queue)
+
+init_op = tf.initialize_all_variables()
+with tf.Session() as sess:
+    sess.run(init_op)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+
+    # 行数がわかっていれば指定して取得可能、複数ファイルの場合は結合して順番に取得
+    # ファイルの順番は保証されない
+    for i in range(8):
+        print sess.run(value)
+
+    coord.request_stop()
+    coord.join(threads)

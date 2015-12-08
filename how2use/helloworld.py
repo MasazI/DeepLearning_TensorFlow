72bd113380050d5a1a471988cf7b6deaf58e25c0
masazdream <masai@masai-no-MacBook-Pro.local>
2015-12-08 19:03:22 +0900

    how2use add match_filenames_once

--- how2use/helloworld.py
+++ how2use/helloworld.py
@@ -12,3 +12,11 @@ print sess.run(a)
 b = tf.constant(20)
 
 print sess.run(a+b)
+
+isess = tf.InteractiveSession()
+files = tf.train.match_filenames_once('/Users/masai/source/tensorflow/how2use/[0-9].txt')
+
+# 変数の初期化
+isess.run(tf.initialize_all_variables())
+print isess.run(files)
+

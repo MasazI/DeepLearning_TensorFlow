9a22a7d5125c6f8ff46b38423bcbbff5e86e3ab1
masazdream <masai@masai-no-MacBook-Pro.local>
2015-12-13 21:17:59 +0900

    add test to create inputs from tfrecords.

--- CNN_tiny/data.py
+++ CNN_tiny/data.py
@@ -11,28 +11,34 @@ IMAGE_WIDTH = FLAGS.image_width
 IMAGE_DEPTH = FLAGS.image_depth
 
 
-def read(tfrecords_file):
-    filename_queue = tf.train.string_input_producer([tfrecords_file])
+def read(filename_queue):
+    '''
+    read from tfrecords file.
+    '''
     class Record(object):
         pass
     result = Record()
+
     # tfrecords reader
     reader = tf.TFRecordReader()
-    result.key, serialized_example = reader.read(filename_queue)
+    _, serialized_example = reader.read(filename_queue)
+
     # parse single example
     features = tf.parse_single_example(
         serialized_example,
         dense_keys=['image_raw', 'height', 'width', 'depth', 'label'],
         dense_types=[tf.string, tf.int64, tf.int64, tf.int64, tf.int64]
     )
+
     # image
-    image = tf.decode_raw(features['image_raw'], tf.uint8)
-    result.image = tf.reshape(image, [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH])
-    result.height = IMAGE_HEIGHT
-    result.width = IMAGE_WIDTH
-    result.depth = IMAGE_DEPTH
+    _image = tf.decode_raw(features['image_raw'], tf.uint8)
+    # batch は shape が定義済みであることを求めるため、reshapeする
+    shape = [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH]
+    result.image = tf.reshape(_image, shape)
+
     # normalize
     # image = tf.cast(image, tf.float32) * (1. /255) - 0.5
+
     # label
     result.label = tf.cast(features['label'], tf.int32)
     return result
---
+++ CNN_tiny/data_test.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+
+import os
+import tensorflow.python.platform
+import tensorflow as tf
+import data
+
+
+class DataInputTest(tf.test.TestCase):
+    '''
+    test data inputs from tfrecords.
+    '''
+    def inputs(self):
+        '''
+        create inputs mini batch
+        '''
+        filename_queue = tf.train.string_input_producer(["data/train.tfrecords"], num_epochs=2)
+        result = data.read(filename_queue)
+        images, sparse_labels = tf.train.shuffle_batch(
+            [result.image, result.label], batch_size=1, num_threads=2,
+            capacity=3,
+            # Ensures a minimum amount of shuffling of examples.
+            min_after_dequeue=1)
+        return images, sparse_labels
+
+    def testSimple(self):
+        images, labels = self.inputs()
+        init_op = tf.initialize_all_variables()
+        with self.test_session() as sess:
+            sess.run(init_op)
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            try:
+                step = 0
+                while not coord.should_stop():
+                    # mini-batchの各要素を取得
+                    image, label = sess.run([images, labels])
+                    # ここでは1imageにつき1step
+                    step += 1
+            except tf.errors.OutOfRangeError:
+                print('Done training for %d epochs, %d steps.' % (2, step))
+            finally:
+                # When done, ask the threads to stop.
+                coord.request_stop()
+            coord.join(threads)
+            sess.close()
+            
+
+if __name__ == "__main__":
+     tf.test.main()

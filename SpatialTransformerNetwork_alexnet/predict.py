7acf37bb37ab24def7a076bfd6ba4add9e34b144
MasazI <masaz.dream@gmail.com>
2016-04-22 16:50:22 +0900

    推定処理の改善

--- SpatialTransformerNetwork_alexnet/predict.py
+++ SpatialTransformerNetwork_alexnet/predict.py
@@ -20,11 +20,10 @@ import model
 #import cnn_tiny_settings as settings
 #FLAGS = settings.FLAGS
 
-NUM_CLASSES = 5
-IMAGE_SIZE = 24
-IMAGE_PIXELS = IMAGE_SIZE*IMAGE_SIZE*3
 
-# 学習時のbatch size
+# settings
+import settings
+FLAGS = settings.FLAGS
 BATCH_SIZE = 1
 
 def _variable_with_weight_decay(name, shape, stddev, wd):
@@ -48,23 +47,36 @@ def _variable_on_cpu(name, shape, initializer):
 
 if __name__ == '__main__':
     test_image = []
-    for i in range(1, len(sys.argv)):
-        image = cv2.imread(sys.argv[i])
-        image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
-        b, g, r = cv2.split(image)       # get b,g,r
-        rgb_image = cv2.merge([r, g, b])     # switch it to rgb
-        test_image.append(rgb_image.flatten().astype(np.float32))
+    for path in range(1, len(sys.argv)):
+        img = cv2.imread(sys.argv[path])
+        h, w, c = np.shape(img)
+        resize_height = FLAGS.image_height
+        resize_width = FLAGS.image_width
+        crop_size = FLAGS.crop_size
+        img = cv2.resize(img, (resize_height, resize_width))
+        img = img.astype(np.float32)
+
+        # Fine Tuningの際は注意すること
+        # img -= self.mean
+        
+        h, w, c = img.shape
+        crop_height, crop_width = ((h-crop_size)/2, (w-crop_size)/2)
+        img = img[crop_height:crop_height+crop_size, crop_width:crop_width+crop_size, :]
+        img = img[None, ...]
+        test_image.append(img)
     test_image = np.asarray(test_image)
-    images_placeholder = tf.placeholder("float", shape=(None, IMAGE_PIXELS))
-    
-    logits = model.inference(images_placeholder)
+    images = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    keep_conv = tf.placeholder(tf.float32)
+    keep_hidden = tf.placeholder(tf.float32)
+
+    logits = model.inference(images, keep_conv, keep_hidden)
     sess = tf.InteractiveSession()
 
     saver = tf.train.Saver()
     sess.run(tf.initialize_all_variables())
 
     # restore trained model
-    ckpt = tf.train.get_checkpoint_state('train')
+    ckpt = tf.train.get_checkpoint_state('train_save')
     print(ckpt.model_checkpoint_path)
     if ckpt and ckpt.model_checkpoint_path:
         saver.restore(sess, ckpt.model_checkpoint_path)
@@ -77,7 +89,7 @@ if __name__ == '__main__':
     print('the num of test images: %d' % (len(test_image)))
     for i in range(len(test_image)):
         start_time = time.time()
-        softmax = logits.eval(feed_dict={images_placeholder: [test_image[i]]}) [0]
+        softmax = logits.eval(feed_dict={images: test_image[i], keep_conv: 1.0, keep_hidden: 1.0}) [0]
         print(softmax)
         pred = np.argmax(softmax)
         duration = time.time() - start_time

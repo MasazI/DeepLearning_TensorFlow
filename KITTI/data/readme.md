0a989313414084a03b6eacf626b3f4428e439d22
masazdream <masaz.dream@gmail.com>
2016-05-10 15:02:47 +0900

    KITTI Depth

---
+++ KITTI/data/readme.md
@@ -0,0 +1,8 @@
+Download KITTI  
+http://www.cvlibs.net/datasets/kitti/raw_data.php  
+  
+2011_09_26_drive_0001 (0.4 GB)  
+Length: 114 frames (00:11 minutes)  
+Image resolution: 1392 x 512 pixels  
+Labels: 12 Cars, 0 Vans, 0 Trucks, 0 Pedestrians, 0 Sitters, 2 Cyclists, 1 Trams, 0 Misc  
+Downloads: [unsynced+unrectified data] [synced+rectified data] [calibration] [tracklets]  
---
+++ KITTI/kitti_to_records.py
@@ -0,0 +1,74 @@
+#encoding: utf-8
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data import image_shape
+from data import get_drive_dir, Calib, get_inds, image_shape, get_calib_dir
+
+
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(root_dir, 'data')
+
+VELODYNE_DIR = "2011_09_26/2011_09_26_drive_0001_sync/velodyne_points"
+
+def get_velodyne_points(velodyne_dir, frame):
+    points_path = os.path.join(velodyne_dir, "data/%010d.bin" % frame)
+    points = np.fromfile(points_path, dtype=np.float32).reshape(-1, 4)
+    points = points[:, :3]  # exclude luminance
+    return points
+
+def load_disparity_points(velodyne_dir, frame, color=False, **kwargs):
+
+    calib = Calib(color=color, **kwargs)
+
+    # read velodyne points
+    points = get_velodyne_points(velodyne_dir, frame)
+
+    # remove all points behind image plane (approximation)
+    points = points[points[:, 0] >= 1, :]
+
+    # convert points to each camera
+    xyd = calib.velo2disp(points)
+
+    # take only points that fall in the first image
+    xyd = calib.filter_disps(xyd)
+
+    return xyd
+
+def test():
+    for i in xrange(108):
+        velodyne_dir = os.path.join(data_dir, VELODYNE_DIR)
+        points = get_velodyne_points(velodyne_dir, i)
+        print("points data type: %s" % type(points))
+        print(points.shape)
+
+        # x
+        print("x max: %f, min: %f" % (np.max(points[:,0]), np.min(points[:,0])))
+        # y
+        print("y max: %f, min: %f" % (np.max(points[:,1]), np.min(points[:,1])))
+        # z
+        print("z max: %f, min: %f" % (np.max(points[:,2]), np.min(points[:,2])))
+
+        # reflectance
+        #print("r max: %f, min: %f" % (np.max(points[:,3]), np.min(points[:,3])))
+
+        image_array = np.asarray([1224, 368])
+
+        xyd = load_disparity_points(velodyne_dir, i, color=False)
+        disp = np.zeros(image_shape, dtype=np.uint8)
+        for x, y, d in np.round(xyd):
+            disp[y, x] = d
+
+
+
+        plt.figure(1)
+        plt.clf()
+        plt.imshow(disp)
+        plt.show()
+
+
+if __name__ == '__main__':
+    test()
